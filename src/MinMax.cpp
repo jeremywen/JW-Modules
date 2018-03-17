c89a139fb0f5ce61d5d186ab88e87ad76bf9aa12
Jeremy Wentworth <jeremy@jeremywentworth.com>
2018-03-17 15:49:14 -0400

    fixed minmax for 0.6

--- src/MinMax.cpp
+++ src/MinMax.cpp
@@ -71,32 +71,32 @@ void MinMax::step() {
 	// Are we waiting on the next trigger?
 	if (bufferIndex >= BUFFER_SIZE) {
 		// Trigger immediately if external but nothing plugged in, or in Lissajous mode
-		// if (lissajous) {
-		// 	bufferIndex = 0;
-		// 	frameIndex = 0;
-		// 	return;
-		// }
+		if (lissajous) {
+			bufferIndex = 0;
+			frameIndex = 0;
+			return;
+		}
 
-		// // Reset the Schmitt trigger so we don't trigger immediately if the input is high
-		// if (frameIndex == 0) {
-		// 	resetTrigger.reset();
-		// }
-		// frameIndex++;
+		// Reset the Schmitt trigger so we don't trigger immediately if the input is high
+		if (frameIndex == 0) {
+			resetTrigger.reset();
+		}
+		frameIndex++;
 
-		// // Must go below 0.1V to trigger
+		// Must go below 0.1V to trigger
 		// resetTrigger.setThresholds(params[TRIG_PARAM].value - 0.1, params[TRIG_PARAM].value);
-		// float gate = inputs[X_INPUT].value;
+		float gate = inputs[X_INPUT].value;
 
-		// // Reset if triggered
-		// float holdTime = 0.1;
-		// if (resetTrigger.process(gate) || (frameIndex >= engineGetSampleRate() * holdTime)) {
-		// 	bufferIndex = 0; frameIndex = 0; return;
-		// }
+		// Reset if triggered
+		float holdTime = 0.1;
+		if (resetTrigger.process(gate) || (frameIndex >= engineGetSampleRate() * holdTime)) {
+			bufferIndex = 0; frameIndex = 0; return;
+		}
 
-		// // Reset if we've waited too long
-		// if (frameIndex >= engineGetSampleRate() * holdTime) {
-		// 	bufferIndex = 0; frameIndex = 0; return;
-		// }
+		// Reset if we've waited too long
+		if (frameIndex >= engineGetSampleRate() * holdTime) {
+			bufferIndex = 0; frameIndex = 0; return;
+		}
 	}
 }
 
