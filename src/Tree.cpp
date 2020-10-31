d399e578ebc5edf2a53fcfe82d5b38397ef1d598
Jeremy Wentworth <jeremy@jeremywentworth.com>
2020-10-31 17:25:31 -0400

    tree

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include "JWModules.hpp"
 
+#define RND_NUMS 25
 struct Tree : Module {
 	enum ParamIds {
 		ANGLE_PARAM,
@@ -9,6 +10,7 @@ struct Tree : Module {
 		REDUCE_PARAM,
 		LENGTH_PARAM,
 		HEIGHT_PARAM,
+		RND_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
@@ -17,6 +19,7 @@ struct Tree : Module {
 		REDUCE_INPUT,
 		LENGTH_INPUT,
 		HEIGHT_INPUT,
+		RND_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
@@ -26,22 +29,41 @@ struct Tree : Module {
 		NUM_LIGHTS
 	};
 
+	float rnd[RND_NUMS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	dsp::SchmittTrigger rndTrigger;
+
 	Tree() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(ANGLE_PARAM, 0.0, 90.0, 10.0, "Angle");
-		configParam(HUE_PARAM, 0.0, 1.0, 0.59, "Color");
+		configParam(ANGLE_PARAM, 0.0, 90.0, 25.0, "Angle");
+		configParam(HUE_PARAM, 0.0, 0.5, 0.1, "Color");
 		configParam(REDUCE_PARAM, 0.1, 0.68, 0.66, "Reduce");
-		configParam(LENGTH_PARAM, 10.0, 200.0, 130, "Length");
-		configParam(HEIGHT_PARAM, 10.0, 250.0, 120, "Height");
+		configParam(LENGTH_PARAM, 10.0, 200.0, 50, "Length");
+		configParam(HEIGHT_PARAM, 10.0, 250.0, 100, "Height");
+		configParam(RND_PARAM, 0.0, 1.0, 0.0, "Jitter");
 	}
 
 	~Tree() {
 	}
 
+	void onReset() override {
+		for(int i=0;i<RND_NUMS;i++){
+			rnd[i] = 0;
+		}
+	}
+
 	void onRandomize() override {
 	}
 
+	void generateRnd() {
+		for(int i=0;i<RND_NUMS;i++){
+			rnd[i] = (random::uniform() - 0.5) * 2;
+		}
+	}
+
 	void process(const ProcessArgs &args) override {
+		if (rndTrigger.process(inputs[RND_INPUT].getVoltage())) {
+			generateRnd();
+		}
 	}
 };
 
@@ -50,7 +72,6 @@ struct TreeDisplay : TransparentWidget {
 	float theta;
 	TreeDisplay(){}
 
-	//https://processing.org/examples/tree.html
 	void draw(const DrawArgs &args) override {
 		//background
 		nvgFillColor(args.vg, nvgRGB(0, 0, 0));
@@ -58,8 +79,7 @@ struct TreeDisplay : TransparentWidget {
 		nvgRect(args.vg, 0, 0, box.size.x, box.size.y);
 		nvgFill(args.vg);
 
-		nvgStrokeColor(args.vg, nvgRGB(255, 255, 255));
-		float v = 5;
+		float v = 2.5;
 		if(module){
 			float inputOffset = module->inputs[Tree::ANGLE_INPUT].isConnected() ? module->inputs[Tree::ANGLE_INPUT].getVoltage() : 0.0;
 			v = clampfjw(module->params[Tree::ANGLE_PARAM].getValue()/9.0 + inputOffset, 0, 10.0);
@@ -67,62 +87,71 @@ struct TreeDisplay : TransparentWidget {
 		float ang = rescalefjw(v, 0, 10, 0.0, 90.0);
 		theta = nvgDegToRad(ang);
 
-		float hue = 0.0;
+		float hue = 0.1;
 		if(module){
 			float inputOffset = module->inputs[Tree::HUE_INPUT].isConnected() ? module->inputs[Tree::HUE_INPUT].getVoltage() : 0.0;
 			hue = clampfjw(module->params[Tree::HUE_PARAM].getValue() + inputOffset/10, 0, 1.0);
 		}
-		float reduce = 0.45;
+		float reduce = 0.65;
 		if(module){
 			float inputOffset = module->inputs[Tree::REDUCE_INPUT].isConnected() ? module->inputs[Tree::REDUCE_INPUT].getVoltage() : 0.0;
 			reduce = clampfjw(module->params[Tree::REDUCE_PARAM].getValue() + rescale(inputOffset,-5,5,0.05,0.33), 0.1, 0.66);
 		}
-		float length = 0.45;
+		float length = 110.0;
 		if(module){
 			float inputOffset = module->inputs[Tree::LENGTH_INPUT].isConnected() ? module->inputs[Tree::LENGTH_INPUT].getVoltage() : 0.0;
 			length = clampfjw(module->params[Tree::LENGTH_PARAM].getValue() + rescale(inputOffset,-5,5,5,100), 10.0, 200.0);
 		}
-		float height = 120.0;
+		float height = 150.0;
 		if(module){
 			float inputOffset = module->inputs[Tree::HEIGHT_INPUT].isConnected() ? module->inputs[Tree::HEIGHT_INPUT].getVoltage() : 0.0;
 			height = clampfjw(module->params[Tree::HEIGHT_PARAM].getValue() + rescale(inputOffset,-5,5,5,125), 10.0, 250.0);
 		}
+		//https://processing.org/examples/tree.html
 		nvgTranslate(args.vg, box.size.x * 0.5, box.size.y);
+		// nvgStrokeColor(args.vg, nvgRGB(255, 255, 255));
+		// nvgStrokeColor(args.vg, nvgHSLA(0.1, 0.5, 1, 0xc0));
 		nvgStrokeColor(args.vg, nvgHSLA(hue, 0.5, 0.5, 0xc0));
-		nvgStrokeWidth(args.vg, 1);
+		int strokeW = 2;
+		nvgStrokeWidth(args.vg, strokeW);
 		nvgBeginPath(args.vg);
 		nvgMoveTo(args.vg, 0, 0);
 		nvgLineTo(args.vg, 0, -height);
 		nvgStroke(args.vg);
-
 		nvgTranslate(args.vg, 0, -height);
 
-		branch(args, length, reduce);
+		branch(args, length, reduce, 1, strokeW, hue);
 	}
 
-	void branch(const DrawArgs &args, float dist, float reduce){
+	void branch(const DrawArgs &args, float dist, float reduce, int count, int strokeW, float hue){
 		dist *= reduce;
 		if (dist > 2) {
+			// DEBUG("count*0.001=%f",count*0.001);
+			count++;
+			float rnd1 = module ? module->rnd[count % RND_NUMS] : 0;
+			float rnd2 = module ? module->rnd[count+1 % RND_NUMS] : 0;
+			nvgStrokeColor(args.vg, nvgHSLA(hue * count * 0.5, 0.5, 0.5, 0xc0));
+
 			nvgSave(args.vg);
-			nvgRotate(args.vg, theta);
-			nvgStrokeWidth(args.vg, 1);
+			nvgRotate(args.vg, theta + rnd1);
+			nvgStrokeWidth(args.vg, strokeW);
 			nvgBeginPath(args.vg);
 			nvgMoveTo(args.vg, 0, 0);
 			nvgLineTo(args.vg, 0, -dist);
 			nvgStroke(args.vg);
 			nvgTranslate(args.vg, 0, -dist);
-			branch(args, dist, reduce);
+			branch(args, dist, reduce, count, strokeW, hue);
 			nvgRestore(args.vg);
 
 			nvgSave(args.vg);
-			nvgRotate(args.vg, -theta);
-			nvgStrokeWidth(args.vg, 1);
+			nvgRotate(args.vg, -theta + rnd1);
+			nvgStrokeWidth(args.vg, strokeW);
 			nvgBeginPath(args.vg);
 			nvgMoveTo(args.vg, 0, 0);
 			nvgLineTo(args.vg, 0, -dist);
 			nvgStroke(args.vg);
 			nvgTranslate(args.vg, 0, -dist);
-			branch(args, dist, reduce);
+			branch(args, dist, reduce, count, strokeW, hue);
 			nvgRestore(args.vg);
 		}
 	}
@@ -132,6 +161,17 @@ struct TreeWidget : ModuleWidget {
 	TreeWidget(Tree *module); 
 	TreeDisplay *display;
 	void appendContextMenu(Menu *menu) override;
+};
+
+struct RandomizeButton : TinyButton {
+	void onButton(const event::Button &e) override {
+		TinyButton::onButton(e);
+		if(e.action == GLFW_PRESS && e.button == GLFW_MOUSE_BUTTON_LEFT){
+			TreeWidget *tw = this->getAncestorOfType<TreeWidget>();
+			Tree *twm = dynamic_cast<Tree*>(tw->module);
+			twm->generateRnd();
+		}
+	}
 };
 
 TreeWidget::TreeWidget(Tree *module) {
@@ -164,6 +204,9 @@ TreeWidget::TreeWidget(Tree *module) {
 
 	addInput(createInput<TinyPJ301MPort>(Vec(165, 360), module, Tree::HUE_INPUT));
 	addParam(createParam<JwTinyKnob>(Vec(180, 360), module, Tree::HUE_PARAM));
+
+	addInput(createInput<TinyPJ301MPort>(Vec(205, 360), module, Tree::RND_INPUT));
+	addParam(createParam<RandomizeButton>(Vec(220, 360), module, Tree::RND_PARAM));
 
 }
 
