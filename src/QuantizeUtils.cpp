997f35f446d77b71c8209e14f210b9e48e5b2b37
Jeremy Wentworth <jeremy@jeremywentworth.com>
2020-11-09 17:20:50 -0500

    fixed lydian

--- src/QuantizeUtils.cpp
+++ src/QuantizeUtils.cpp
@@ -3,6 +3,7 @@
 struct QuantizeUtils {
 
 	//copied & fixed these scales http://www.grantmuller.com/MidiReference/doc/midiReference/ScaleReference.html
+	//more scales http://lawriecape.co.uk/theblog/index.php/archives/881
 	int SCALE_AEOLIAN        [8] = {0, 2, 3, 5, 7, 8, 10, 12};
 	int SCALE_BLUES          [7] = {0, 3, 5, 6, 7, 10, 12}; //FIXED!
 	int SCALE_CHROMATIC      [13]= {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
@@ -11,7 +12,7 @@ struct QuantizeUtils {
 	int SCALE_HARMONIC_MINOR [8] = {0, 2, 3, 5, 7, 8, 11, 12};
 	int SCALE_INDIAN         [8] = {0, 1, 1, 4, 5, 8, 10, 12};
 	int SCALE_LOCRIAN        [8] = {0, 1, 3, 5, 6, 8, 10, 12};
-	int SCALE_LYDIAN         [8] = {0, 2, 4, 6, 7, 9, 10, 12};
+	int SCALE_LYDIAN         [8] = {0, 2, 4, 6, 7, 9, 11, 12};
 	int SCALE_MAJOR          [8] = {0, 2, 4, 5, 7, 9, 11, 12};
 	int SCALE_MELODIC_MINOR  [10] = {0, 2, 3, 5, 7, 8, 9, 10, 11, 12};
 	int SCALE_MINOR          [8] = {0, 2, 3, 5, 7, 8, 10, 12};
